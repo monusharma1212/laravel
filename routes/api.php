6ac8963a7e0cf21622b09f98b70f7ed8bc25301b
monusharma1212 <monukumarsharma070@gmail.com>
2026-02-26 17:52:05 +0530

    26-02-2026  updated

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register',[UserApiController::class,'register']);
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/userlist',[UserApiController::class,'userlist']);
+    Route::put('/user/{id}',[UserApiController::class,'view']);
+});
